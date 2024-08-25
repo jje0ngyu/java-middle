291f21024fcc1c4caf3f080bbf0da98d0dc247ca
jje0ngyu <naniaerang@naver.com>
2024-08-25 17:00:17 +0900

    Wrapper - Auto-Boxing

--- src/lang/wrapper/AutoboxingMain1.java
+++ src/lang/wrapper/AutoboxingMain1.java
@@ -3,11 +3,11 @@ package lang.wrapper;
 public class AutoboxingMain1 {
 
     public static void main(String[] args) {
-        // Primitive -> Wrapper
+        // Primitive -> Wrapper (Boxing)
         int value = 7;
         Integer boxedValue = Integer.valueOf(value);
 
-        // Wrapper -> Primitive
+        // Wrapper -> Primitive (Unboxing)
         int unboxedValue = boxedValue.intValue();
 
         System.out.println("boxedValue = " + boxedValue);
